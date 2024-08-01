82ed1ce5dbd638c2558c59a14e4f7e4926fb1440
ferdinand7103 <84262024+ferdinand7103@users.noreply.github.com>
2024-08-01 18:42:33 +0700

    MVVM - Create

---
+++ HXD/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  HXD
+//
+//  Created by Ferdinand Jacques on 01/08/24.
+//
+
+import Foundation
---
+++ HXD/Views/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  HXD
+//
+//  Created by Ferdinand Jacques on 01/08/24.
+//
+
+import Foundation
