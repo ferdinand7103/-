ab55e8a62c6cb9fccecfc2b0dc2c877d749e04d4
ferdinand7103 <84262024+ferdinand7103@users.noreply.github.com>
2024-08-01 18:40:36 +0700

    New Repo

---
+++ HXD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HXD
+//
+//  Created by Ferdinand Jacques on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HXD/HXDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HXDApp.swift
+//  HXD
+//
+//  Created by Ferdinand Jacques on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HXDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
