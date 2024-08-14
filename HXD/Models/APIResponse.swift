86c4b699aeddd39f036649929751cca9585aeabb
Vincent Junior <Vincent.halimjr@gmail.com>
2024-08-14 23:15:06 +0700

    add AI backend end + recording

---
+++ HXD/Models/APIResponse.swift
@@ -0,0 +1,13 @@
+//
+//  APIResponse.swift
+//  HXD
+//
+//  Created by Vincent Junior Halim on 14/08/24.
+//
+
+import Foundation
+
+
+struct APIResponse: Codable {
+    let text: String
+}
---
+++ HXD/Services/HuggingFace/HuggingFace.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+class HuggingFace {
+    
+    // Convert audio file to Data
+    func convertAudioToData(audioPath: String) -> Data? {
+        let url = URL(fileURLWithPath: audioPath)
+        
+        do {
+            let audioData = try Data(contentsOf: url)
+            return audioData
+        } catch {
+            print("Error loading audio file: \(error)")
+            return nil
+        }
+    }
+    
+    // Fetch response from API
+    func getResponse(audioPath: String) async -> String? {
+        guard let audioData = convertAudioToData(audioPath: audioPath) else {
+            print("Failed to convert audio to data")
+            return nil
+        }
+        
+        var request = URLRequest(url: URL(string: "https://api-inference.huggingface.co/models/jonatasgrosman/wav2vec2-large-xlsr-53-chinese-zh-cn")!, timeoutInterval: Double.infinity)
+        request.addValue("Bearer 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", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = audioData
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            return await getAnswer(data: data)
+        } catch {
+            print("Error during API request: \(error)")
+            return nil
+        }
+    }
+    
+    // Decode JSON response
+    func getAnswer(data: Data) async -> String? {
+        do {
+            let answer = try JSONDecoder().decode(APIResponse.self, from: data)
+            return answer.text
+        } catch {
+            print("Error decoding JSON: \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
