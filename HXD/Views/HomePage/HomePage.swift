b51d4f9566358457268127b8a78f9026d63e9f16
Vincent Junior <Vincent.halimjr@gmail.com>
2024-08-15 17:45:08 +0700

    homePage

---
+++ HXD/Views/HomePage/HomePage.swift
@@ -0,0 +1,8 @@
+//
+//  HomePage.swift
+//  HXD
+//
+//  Created by Vincent Junior Halim on 15/08/24.
+//
+
+import Foundation
---
+++ HXD/Views/Reuseable/RectangleButton.swift
@@ -0,0 +1,8 @@
+//
+//  RectangleButton.swift
+//  HXD
+//
+//  Created by Vincent Junior Halim on 15/08/24.
+//
+
+import Foundation
