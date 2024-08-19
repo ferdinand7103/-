d92fb63fb3b1a01267887e4b6fec78cc8ab1181b
ferdinand7103 <84262024+ferdinand7103@users.noreply.github.com>
2024-08-19 11:01:11 +0700

    fix json format

---
+++ HXD/Views/StoryView.swift
@@ -0,0 +1,21 @@
+//
+//  StoryView.swift
+//  HXD
+//
+//  Created by Rama Eka Hartono on 19/08/24.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    @ObservedObject var viewModel = StoryViewModel()
+    @StateObject private var talkingAnimation = TalkingAnimation()
+
+    var body: some View {
+        Text("hell")
+    }
+}
+
+#Preview {
+    StoryView()
+}
