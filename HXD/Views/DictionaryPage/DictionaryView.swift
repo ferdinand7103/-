bf078ef68c91e1b9d774dd18523d387d0d88795b
Sasutora <112454542+Sasutora@users.noreply.github.com>
2024-08-20 10:27:26 +0700

    dict

---
+++ HXD/Views/DictionaryPage/DictionaryView.swift
@@ -0,0 +1,65 @@
+//
+//  DictionaryView.swift
+//  HXD
+//
+//  Created by Rama Eka Hartono on 20/08/24.
+//
+
+import SwiftUI
+
+struct DictionaryView: View {
+    @ObservedObject var viewModel: StoryViewModel
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Dictionary")
+                    .font(.title)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(.top, 16)
+                
+                Text("Story 1 - Chapter 2")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                
+
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 0) {
+                    ForEach(0..<viewModel.currentStory.flashcard.count) { index in
+                        ZStack {
+                            FlashcardView(vocab: viewModel.currentStory.flashcard[index], width: 170, height: 236)
+                                .padding(0)
+                                .shadow(radius: 2)
+                            
+                            SoundButton(vocab: viewModel.currentStory.flashcard[index])
+                                .frame(width: 30, height: 30)
+                                .background(Color.white)
+                                .clipShape(Circle())
+                                .offset(x: 20, y: 75) // Position the button on the bottom-right corner
+                        }
+                    }
+                }
+                .padding(.horizontal, 16)
+                
+                Spacer()
+            }
+            .navigationBarItems(leading: Button(action: {
+                // Handle the back button action here
+            }) {
+                HStack {
+                    Image(systemName: "chevron.left")
+                    Text("Back")
+                }
+                .foregroundColor(.orange)
+            })
+        }
+    }
+}
+
+
+
+#Preview {
+        DictionaryView(viewModel: StoryViewModel())
+    }
+
