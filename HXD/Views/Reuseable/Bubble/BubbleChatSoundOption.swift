bd0fff7365f726f54ca5250901329fa582fbef59
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-08-20 19:39:21 +0700

    BubbleChatSoundOption done

--- HXD/Views/Reuseable/Bubble/BubbleChatSoundOption.swift
+++ HXD/Views/Reuseable/Bubble/BubbleChatSoundOption.swift
@@ -11,17 +11,29 @@ struct BubbleChatSoundOption: View {
     var text: String
     var bubbleColor: Color = .white
     var textColor: Color = .black
+    let tts = TextToSpeechServices()
 
     var body: some View {
         VStack(spacing: 0) {
-            HStack {
+            VStack(alignment: .leading) {
                 Text(text)
-                    .padding(16)
+                    .padding()
                     .foregroundColor(textColor)
-                    .font(.system(size: 20, weight: .bold))
-                Spacer(minLength: 0)
+                    .font(.system(size: 20, weight: .semibold))
+
+                HStack {
+                    BubbleSoundButton(icon: "sound") {
+                        tts.speak(text: text)
+                    }
+                        .padding(.trailing, 10)
+                    BubbleSoundButton(icon: "soundSlow") {
+                        tts.speakSlow(text: text)
+                    }
+                }
+                .padding(EdgeInsets(top: 0, leading: 16, bottom: 16, trailing: 16))
             }
-            .frame(minWidth: 300, maxWidth: 300, alignment: .leading)
+            .padding(8)
+            .frame(minWidth: 350, maxWidth: 350, alignment: .leading)
             .background(bubbleColor)
             .clipShape(RoundedRectangle(cornerRadius: 16))
             .overlay(
@@ -31,11 +43,18 @@ struct BubbleChatSoundOption: View {
             BubbleChatTail(pos: "A")
                 .fill(bubbleColor)
                 .frame(width: 30, height: 15)
-                .offset(y: -5)
+                .offset(x: -30, y: -5)
         }
     }
 }
 
 #Preview {
-    BubbleChatSoundOption(text: "Now, repeat after me: 你好 (Nǐ hǎo).", bubbleColor: .blue)
+    BubbleChatSoundOption(
+        text: """
+        你好，请问，你们几位？
+        Nǐ hǎo, qǐng wèn, nǐmen jǐ wèi?
+        Hi, may I ask, how many people are you?
+        """,
+        bubbleColor: .redLight
+    )
 }
