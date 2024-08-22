be4b23a50921f23c87e76dd1350ab036b25a805d
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-08-22 10:33:33 +0700

    PinYin NavigationButton UI done

---
+++ HXD/Views/PinYinPage/LearnPinYinPage.swift
@@ -0,0 +1,18 @@
+//
+//  LearnPinYinPage.swift
+//  HXD
+//
+//  Created by Michelle Angela Aryanto on 22/08/24.
+//
+
+import SwiftUI
+
+struct LearnPinYinPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnPinYinPage()
+}
